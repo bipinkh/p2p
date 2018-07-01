e42741964c3c0b4d9580f3f18c68483a30ec69b3
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2018-07-01 23:24:45 +0545

    Minor update in client endPoints

--- src/main/java/com/endtoendmessenging/controller/ClientApiController.java
+++ src/main/java/com/endtoendmessenging/controller/ClientApiController.java
@@ -1,5 +1,6 @@
 package com.endtoendmessenging.controller;
 
+import com.endtoendmessenging.model.remote.ErrorBean;
 import com.endtoendmessenging.repository.MessageRepository;
 import com.endtoendmessenging.repository.SubscriptionRepository;
 import com.endtoendmessenging.service.KademliaAdapterService;
@@ -18,7 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.concurrent.TimeoutException;
 
 @RestController
@@ -38,28 +38,35 @@ public class ClientApiController {
     @GetMapping("/subscribe")
     public ResponseEntity<?> subscribe(@RequestParam(value = "nodeid") String nodeid, @RequestParam(value = "clientid") String clientid) {
         try {
+            try {
+                Key node = new Key(kademliaService.getValue(new Key(clientid)));
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("You have already registered with " + node.toString());
+            } catch (KadProtocol.ContentNotFoundException e) {
+                e.printStackTrace();
+            }
             if (kademliaService.subscribe(new Key(clientid), new Key(nodeid), 3600 * 24 * 30))
-                return ResponseEntity.ok("Success");
+                return ResponseEntity.ok(true);
         } catch (KademliaExtendedDHT.NoSuchNodeFoundException e) {
             logger.info("/subscribe  -> Node not found : " + nodeid);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The specified node doesn't exist in the network");
+            return new ResponseEntity<>(new ErrorBean("The specified node doesn't exist in the network"), HttpStatus.BAD_REQUEST);
 
         } catch (TimeoutException e) {
             logger.info("/subscribe  -> Timeout connecting to node : " + nodeid);
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The specified node Didn't respond");
+            return new ResponseEntity<>(new ErrorBean("The specified node Didn't respond"), HttpStatus.BAD_REQUEST);
+        } catch (ServerShutdownException ignored) {
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Error while processing your request");
+        return new ResponseEntity<>(new ErrorBean("Unexpected Error while processing your request"), HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
     @RequestMapping("/unsubscribe")
-    public ResponseEntity<String> unsubscribe(@RequestParam(value = "clientid") String id) {
+    public ResponseEntity unsubscribe(@RequestParam(value = "clientid") String id) {
         try {
             if (new Key(kademliaService.getValue(new Key(id))).equals(kademliaService.getLocalKey())) {
                 if (subscriptionRepository.findById(new Key(id).toBytes()).isPresent()) {
                     subscriptionRepository.deleteById(new Key(id).toBytes());
-                    return ResponseEntity.ok("Success");
+                    return ResponseEntity.ok(true);
                 }
             } else {
                 //todo: give the unsubscribe message to the postman of the client.
@@ -67,13 +74,13 @@ public class ClientApiController {
         } catch (ServerShutdownException e) {
             e.printStackTrace();
         } catch (KadProtocol.ContentNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client is not subscribed to any node");
+            return new ResponseEntity<>(new ErrorBean("Client is not subscribed to any node"), HttpStatus.NOT_FOUND);
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Error while processing your request");
+        return new ResponseEntity<>(new ErrorBean("Unexpected Error while processing your request"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping(value = "/get_subscribed", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getSubscribed(@RequestParam String clientid) {
+    public ResponseEntity getSubscribed(@RequestParam String clientid) {
         try {
             if (subscriptionRepository.findById(new Key(clientid).toBytes()).isPresent()) {
                 return ResponseEntity.ok(kademliaService.getLocalKey().toString());
@@ -84,7 +91,7 @@ public class ClientApiController {
         } catch (com.soriole.kademlia.core.KadProtocol.ContentNotFoundException ignored) {
             return ResponseEntity.ok("null");
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected Error while processing your request");
+        return new ResponseEntity<>(new ErrorBean("Unexpected Error while processing your request"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("get_peers")
@@ -94,26 +101,26 @@ public class ClientApiController {
     }
 
     @GetMapping("send_message")
-    public ResponseEntity<String> sendMessage(@RequestParam String clientid, @RequestParam String message, @RequestParam String receiver) {
+    public ResponseEntity sendMessage(@RequestParam String clientid, @RequestParam String message, @RequestParam String receiver) {
         TextMessage txtMessage = new TextMessage(message);
         txtMessage.sender = new Key(clientid).toBytes();
         try {
 
             if (kademliaService.sendToClient(new Key(receiver).toBytes(), txtMessage.toBytes())) {
-                return ResponseEntity.ok("Success");
+                return ResponseEntity.ok(true);
             } else {
-                return new ResponseEntity<>("Unexpected Error while processing your request", HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>(new ErrorBean("Unexpected Error while processing your request"), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (KademliaExtendedDHT.NoSuchNodeFoundException e) {
             logger.info("/send_message  --> Client `" + receiver + "` has subscription but the node is not found");
-            return new ResponseEntity<>("The Postmen of client is not found in the network", HttpStatus.EXPECTATION_FAILED);
+            return new ResponseEntity<>(new ErrorBean("The Postmen of client is not found in the network"), HttpStatus.EXPECTATION_FAILED);
         } catch (TimeoutException e) {
             logger.info("/send_message  --> Client `" + receiver + "` has subscription but the node didn't reply");
-            return new ResponseEntity<>("The Postmen was discovered but didn't rely", HttpStatus.EXPECTATION_FAILED);
+            return new ResponseEntity<>(new ErrorBean("The Postmen was discovered but didn't rely"), HttpStatus.EXPECTATION_FAILED);
 
         } catch (KadProtocol.ContentNotFoundException e) {
             logger.info("/send_message  --> Client `" + receiver + "` not found in the network");
-            return new ResponseEntity<>("The client doesn't use our service", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ErrorBean("The client hasn't registered for our service"), HttpStatus.BAD_REQUEST);
 
         }
     }
@@ -126,7 +133,7 @@ public class ClientApiController {
         if (this.subscriptionRepository.findById(new Key(clientId).toBytes()).isPresent()) {
             return ResponseEntity.ok(MessageCollecitonBean.fromMessageCollection(messageRepository.findByReceiver(new Key(clientId).toBytes())));
         } else {
-            return new ResponseEntity<>("The client doesn't use our service", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ErrorBean("The client doesn't use our service"), HttpStatus.BAD_REQUEST);
         }
     }
 
